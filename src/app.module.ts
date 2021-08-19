7c44cda96e54ab784cf731d4b61bb0e98ebd2b7c
Michael Owolabi <michael.i.owolabi@gmail.com>
2021-08-19 08:16:17 +0100

    write e2e test for mutation

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+      debug: true,
+      
+    }),
+  ],
+})
+export class AppModule {}
